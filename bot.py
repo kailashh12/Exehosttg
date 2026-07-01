f9019703bf865c6d2d06cb36971565f60360b556
kailashh12 <kamatvinod775@gmail.com>
2026-07-01 12:19:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -449,4 +449,4 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
+    socketio.run(app, host='0.0.0.0', port=80, debug=True)
